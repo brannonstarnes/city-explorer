17226bf76bde5a1e40d95dc7b9a8282d71d80286
Brannon Starnes <brannlee808@gmail.com>
2021-11-01 22:27:09 -0700

    add getMap function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { Component } from 'react';
 import CityForm from './components/CityForm.js'
 import CityInfo from './components/CityInfo.js'
+import Map from './components/Map.js'
 import axios from 'axios';
 
 
@@ -17,8 +18,13 @@ export default class App extends Component {
 
 //when event occurs in input field, update state to match the value of the input field
 handleChange = (e) => {
-  this.setState({locationName: e.target.value})
+  this.setState({locationName: e.target.value.toUpperCase()})
 };
+
+getMap = async () => {
+  const url = `https://tiles.locationiq.com/v3/hybrid/vector.json?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.lat},${this.state.lon}&zoom=1-18`;
+
+}
 
 handleClick = async () => {
   const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.locationName}&format=json`;
@@ -27,6 +33,9 @@ handleClick = async () => {
   const lat = response.data[0].lat
   const lon = response.data[0].lon
   this.setState({lat: lat, lon: lon});
+
+  
+
 }
 
   render() {
@@ -34,6 +43,7 @@ handleClick = async () => {
     <>
     <CityForm locationName = {this.state.locationName} handleChange = {this.handleChange} handleClick={this.handleClick}/>
     <CityInfo locationName = {this.state.locationName} lat = {this.state.lat} lon={this.state.lon}/>
+    <Map lat = {this.state.lat} lon={this.state.lon} />
     </>
     );
   }

acc21066ebc4f5d0a3fedcf487e84d2291129603
Brannon Starnes <brannlee808@gmail.com>
2021-11-01 20:36:49 -0700

    created submission form, prior to handleClick

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-    location: ''
+    locationName: ''
   }
 }
 
+//when event occurs in input field, update state to match the value of the input field
+handleChange = (e) => {
+  this.setState({locationName: e.target.value})
+};
+
+
   render() {
     return (
-      <div>
-        <input type='text'></input>
-        <h1>Test</h1>
-      </div>
+      <Form>
+  <Form.Group className="mb-3" >
+    <Form.Label>City Name</Form.Label>
+    <Form.Control type="search" placeholder="i.e. Chattanooga" onChange={this.handleChange} />
+    <Form.Text className="text-muted">
+      Please enter a city name to explore:
+    </Form.Text>
+  </Form.Group>
+  <Button variant="primary" onClick="handleClick">
+    Explore!
+  </Button>  
+  <h2>{this.state.locationName}</h2>
+</Form>
     );
   }
 };

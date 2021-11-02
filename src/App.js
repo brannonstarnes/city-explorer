b123976d7d358c49a78bc8d4d4e369a9ff01d0a7
Brannon Starnes <brannlee808@gmail.com>
2021-11-01 21:03:49 -0700

    add handleClick function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 
 export default class App extends Component {
@@ -17,6 +18,12 @@ handleChange = (e) => {
   this.setState({locationName: e.target.value})
 };
 
+handleClick = async () => {
+  const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.locationName}&format=json`;
+
+  let response = await axios.get(url);
+  console.log(response.data);
+}
 
   render() {
     return (
@@ -28,7 +35,7 @@ handleChange = (e) => {
       Please enter a city name to explore:
     </Form.Text>
   </Form.Group>
-  <Button variant="primary" onClick="handleClick">
+  <Button variant="primary" onClick={this.handleClick}>
     Explore!
   </Button>  
   <h2>{this.state.locationName}</h2>
